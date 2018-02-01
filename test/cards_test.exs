f41d54d80c978f8ac70a79fd9b7d1be1b0a087e8
Jeremy Lincoln <bossco28@gmail.com>
2018-02-01 15:49:12 -0500

    Create doc tests and case tests

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -5,4 +5,17 @@ defmodule CardsTest do
   # test "the truth" do
   #   assert 1 + 1 == 2
   # end
+  
+  test "create_deck makes 20 cards" do
+    deck_length = length(Cards.create_deck)
+    assert deck_length == 52
+  end
+
+  test "shuffling a deck randomizes it" do
+    deck = Cards.create_deck
+    refute deck == Cards.shuffle(deck)
+    # ^^ 
+    # instead of assert deck != Cards.shuffle(deck)
+  end
+
 end
